28d65a29f6854ac5a5da36493c4e7b9e648d7a78
Bordin Amelie <amelie.bordin@etudiant.univ-rennes1.fr>
2022-12-05 10:09:14 +0100

    Seance 2
    Tentative de seek

---
+++ TinyLittleWarriors/Assets/Script/boid management/Seek.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : AgentBehavior
+{
+    //move towards a target
+    public override steering GetSteering()
+    {
+        steering steer = new steering();
+        Vector3 relativePos = target.transform.position - transform.position;
+        steer.linear = relativePos;
+        steer.linear.Normalize();
+        steer.linear = steer.linear * agent.maxAccel;
+        steer.angular = Vector3.Angle(relativePos, transform.forward);
+        return steer;
+    }
+}
