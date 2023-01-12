28928d2f9aa12c1a7e1af86a11ba1f53fa809d1e
Leclerc Mathis <mathis.leclerc@etudiant.univ-rennes1.fr>
2023-01-12 14:26:58 +0100

    Update terrain + adding first menu scene

---
+++ TinyLittleWarriors/Assets/Script/Menu/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+    public void SettingsButton()
+    {
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
