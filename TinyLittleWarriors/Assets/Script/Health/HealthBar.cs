9787fae4c011355dcec7dde937ec9e7b8ad7909a
Leclerc Mathis <mathis.leclerc@etudiant.univ-rennes1.fr>
2023-02-07 17:31:55 +0100

    Update HealthBar.cs
    -Adding health bar looking to camera

--- TinyLittleWarriors/Assets/Script/Health/HealthBar.cs
+++ TinyLittleWarriors/Assets/Script/Health/HealthBar.cs
@@ -6,6 +6,12 @@ public class HealthBar : MonoBehaviour
     public Image bar;
     public CharacterStats stats;
 
+    public Camera mainCamera;
+
+    private void Update()
+    {
+        bar.transform.LookAt(bar.transform.position + mainCamera.transform.rotation * Vector3.back, mainCamera.transform.rotation * Vector3.down);
+    }
     private void Start()
     {
         bar.fillAmount = stats.getMaxLife();
